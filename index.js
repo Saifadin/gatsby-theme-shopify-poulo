8c9b229dd09b8ff2f62d4559565c8eaf6846fe79
Osamah <osamah@aldoaiss.de>
2019-02-24 14:17:41 +0100

    🔖 fix exports

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-export { default as tokens } from './src/components/tokens';
-export { default as Navigation } from './src/components/Navigation';
 export { default as Hero } from './src/components/Hero';
+export { default as Section } from './src/components/Section';
+export { default as ProductCardList } from './src/components/ProductCardList';
+
 export { default as Seo } from './src/containers/SeoContainer';
+
 export { colors, layout } from './src/components/tokens';
