51332995b38c2e6eab4f198ab94f4875137f86ca
Osamah <osamah@aldoaiss.de>
2019-03-10 20:04:40 +0100

    Close Nav on navigation

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -2,6 +2,7 @@ import { graphql, useStaticQuery } from 'gatsby';
 import React, { useContext } from 'react';
 
 import { MenuWrapper, PrimaryNavItem, MenuItem, PrimaryNavItemLink, SubMenu, SecondaryNavItem } from './styles';
+import LayoutContext from '../../context/LayoutContext';
 
 const Menu: React.FunctionComponent = () => {
   const { site } = useStaticQuery(graphql`
@@ -27,17 +28,24 @@ const Menu: React.FunctionComponent = () => {
       navigation: { menu },
     },
   } = site;
+  const { setScreen } = useContext(LayoutContext);
 
   return (
     <MenuWrapper>
       {menu.map(({ displayName, link, subMenu = [] }: any) => {
         return (
           <MenuItem key={link}>
-            {link ? <PrimaryNavItemLink to={link}>{displayName}</PrimaryNavItemLink> : <PrimaryNavItem>{displayName}</PrimaryNavItem>}
+            {link ? (
+              <PrimaryNavItemLink to={link} onClick={() => setScreen('main')}>
+                {displayName}
+              </PrimaryNavItemLink>
+            ) : (
+              <PrimaryNavItem>{displayName}</PrimaryNavItem>
+            )}
             {subMenu.length > 0 ? (
               <SubMenu>
                 {subMenu.map(({ displayName: dN2, link: l2 }: any) => (
-                  <SecondaryNavItem key={l2} to={l2}>
+                  <SecondaryNavItem key={l2} to={l2} onClick={() => setScreen('main')}>
                     {dN2}
                   </SecondaryNavItem>
                 ))}
