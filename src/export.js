305f3691326d154eeca4c3de6f54886114a2746b
Osamah <osamah@aldoaiss.de>
2019-06-09 23:40:36 +0200

    Move export.js

---
+++ src/export.js
@@ -0,0 +1,21 @@
+export { default as Hero } from './components/Hero';
+export { default as Section, SectionDescription, SectionTitle, SectionSubTitle } from './components/Section';
+export { default as ProductCardList } from './components/ProductCardList';
+export { default as Button, ButtonLink } from './components/Button';
+export { default as ImageGridTwoLargeFourSmall, LargeChild, SmallChild } from './components/ImageGridTwoLargeFourSmall';
+export { default as ImageGridTwoHalfOneFullwidth, FullwidthChild, OneHalfChild } from './components/ImageGridTwoHalfOneFullwidth';
+export { default as ImageGridTwoHalf } from './components/ImageGridTwoHalf';
+
+export { default as EqualColumns } from './components/EqualColumns';
+export { default as EqualColumn } from './components/EqualColumns/EqualColumn';
+
+export { PrimaryTitle, SecondaryTitle, SubTitle, Paragraph } from './components/Typography';
+
+export { default as Seo } from './containers/SeoContainer';
+export { default as PageLayout } from './containers/PageLayout';
+export { default as ProductPage } from './containers/ProductPage';
+export { default as ErrorPage } from './containers/404';
+
+export { useMedia, useMediaLayout } from './hooks/useMedia';
+
+export { colors, layout, button, typo } from './components/tokens';
