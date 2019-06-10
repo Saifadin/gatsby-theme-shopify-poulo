15ddb299501334ba3435a733b082657a5d28cf4e
Osamah <osamah@aldoaiss.de>
2019-06-10 18:49:54 +0200

    Fix import

--- src/containers/404.tsx
+++ src/containers/404.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
+import { Section, SectionTitle, SectionDescription } from 'page-kit';
 
 import SeoContainer from './SeoContainer';
-import Section, { SectionTitle, SectionDescription } from '../components/Section';
 
 interface ErrorProps {
   title?: React.ReactNode;
